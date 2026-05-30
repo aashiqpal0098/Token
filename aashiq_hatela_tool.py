008179d8f9df2ebed6ad6de13f8dcf3207007b8f
Aashiq <164062140+aashiqpal0098@users.noreply.github.com>
2026-05-30 16:19:43 +0530

    Update aashiq_hatela_tool.py

--- aashiq_hatela_tool.py
+++ aashiq_hatela_tool.py
@@ -1207,7 +1207,8 @@ if __name__ == '__main__':
     print("🍃 Press Ctrl+C to stop\n")
     
     if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
-
-# Render ke liye
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
+# For gunicorn
 app = app
